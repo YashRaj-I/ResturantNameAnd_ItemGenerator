8c18e3454b508ebfa5eb9a83999bfcc6b36de933
YASH RAJ <yr7971556@gmail.com>
2025-06-01 09:01:31 +0530

    fix

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import streamlit as st
 import restaurant_generator as rg
 st.set_page_config(page_title="Restaurant Name Generator", page_icon="🍽️")
 st.title("Restaurant Name Generator 🍽️")
+# add a background image
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://c.ndtvimg.com/2020-09/if4pp5j8_vegetarian_625x300_30_September_20.jpg");
+        background-size: cover;
+        background-position: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 cuisine = st.selectbox("Select a cuisine", ["Indian", "Italian", "Chinese", "Mexican", "French", "Japanese", "Thai", "Mediterranean", "American"])
 
 # Load Gemini model
-model = rg.configure_gemini(r"C:\Users\Asus\Documents\LLM\GeminiapiKey.env")
+model = rg.configure_gemini(r"C:\Users\Asus\Documents\RestaurantNameGenerator\GeminiapiKey.env")
 
 if cuisine:
     response = rg.generate_name_and_items(cuisine, model)
